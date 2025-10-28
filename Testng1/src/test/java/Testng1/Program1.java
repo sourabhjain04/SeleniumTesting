27d3a324d07ed28191a01af1d0278796ff3a7a35
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-28 17:26:38 +0530

    Add files via upload

---
+++ Testng1/src/test/java/Testng1/Program1.java
@@ -0,0 +1,23 @@
+package Testng1;
+
+import org.testng.annotations.Test;
+
+public class Program1 {
+	
+	
+  @Test
+  public void testcase1() {
+	  
+	  System.out.println("This is Test Case 1");
+  }
+  
+  @Test
+  public void testcase2() {
+	  
+	  System.out.println("This is Test Case 2");
+  }
+  
+  
+  
+  
+}
---
+++ Testng1/src/test/java/Testng1/Program2.java
@@ -0,0 +1,19 @@
+package Testng1;
+
+import org.testng.annotations.Test;
+
+public class Program2 {
+	
+  @Test
+  public void testCase3() {
+	  System.out.println("This is Test Case 3");
+	  
+	  
+  }
+  
+  
+  @Test
+  public void testCase4() {
+	  System.out.println("This is Test Case 4");
+  }
+}
