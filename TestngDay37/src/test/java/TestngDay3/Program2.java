f703e5a96dbdfd13b33add38ad087c51422c503b
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-29 17:49:20 +0530

    Add files via upload

---
+++ TestngDay37/src/test/java/TestngDay3/Program2.java
@@ -0,0 +1,28 @@
+package TestngDay3;
+
+
+import org.testng.annotations.Test;
+
+public class Program2 {
+	
+		
+	
+	  @Test
+	  public void PlacementLogin() {
+		  System.out.println("Placement Login");
+		  
+		  
+	  }
+	  
+	  
+	  @Test
+	  public void PlacementBrokenLink() {
+		  System.out.println("Placement Broken Link");
+	  }
+	  
+
+	
+	  
+	
+	
+}
