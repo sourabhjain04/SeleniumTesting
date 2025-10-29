8b6bc1102a286b5341e4b3337ad918710304584e
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-29 17:42:55 +0530

    Add files via upload

---
+++ TestngDay3SwitchWindow/src/main/java/TestngDay3SwitchWindow/App.java
@@ -0,0 +1,13 @@
+package TestngDay3SwitchWindow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestngDay3SwitchWindow/src/test/java/TestngDay3SwitchWindow/WindowHandlerTest.java
@@ -0,0 +1,78 @@
+package TestngDay3SwitchWindow;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class WindowHandlerTest {
+	
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
+
+        driver.get("https://demo.automationtesting.in/Windows.html");  // sample site
+	}
+
+	@Test
+	public void handleWindowExample() throws InterruptedException {
+		
+		// click the link that opens a new tab or window
+		driver.findElement(By.xpath("//*[@id=\"Tabbed\"]/a/button")).click();
+		
+		// get parent window handle
+		
+		String parentWindow = driver.getWindowHandle();
+		
+		//Get all window handles
+		
+		Set<String> allWindows = driver.getWindowHandles();
+		
+		
+	// Use iterator to switch to new Window
+		
+		Iterator<String> itr = allWindows.iterator();
+		
+		while(itr.hasNext()) {
+			
+			String childWindow = itr.next();
+			
+			if(!parentWindow.equalsIgnoreCase(childWindow)) {
+				
+				driver.switchTo().window(childWindow);
+				
+				System.out.println("Child window title: " + driver.getTitle());
+				
+				Thread.sleep(2000);
+				
+				driver.close();  // close child window
+			}
+		}
+		//Switch back to parent window
+		
+		driver.switchTo().window(parentWindow);
+		Thread.sleep(2000);
+		System.out.println("back to parent Window Title: " + driver.getTitle());
+	}
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+}
