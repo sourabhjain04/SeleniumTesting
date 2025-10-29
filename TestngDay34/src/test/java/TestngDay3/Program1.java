00a8a74d850273f47fcd5a1bb91040557e2d9a55
Sourabh Jain <110473469+sourabhjain04@users.noreply.github.com>
2025-10-29 17:47:43 +0530

    Add files via upload

---
+++ TestngDay34/src/test/java/TestngDay3/Program1.java
@@ -0,0 +1,21 @@
+package TestngDay3;
+
+import org.testng.annotations.Test;
+
+public class Program1 {
+	
+	
+	
+	  @Test(enabled = false)
+	  public void AdmissionLogin1() {
+		  
+		  System.out.println("This test case contin the Bug...");
+		  System.out.println("Admission Login");
+	  }
+	  
+	  @Test
+	  public void AdmissionBrokernLinks() {
+		  
+		  System.out.println("Admission Brokern Links");
+	  }
+}
